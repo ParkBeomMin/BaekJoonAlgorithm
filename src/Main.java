ed0e65c39c89a544e843219c15d492bbbd353154
ParkBeomMin <club20608@gmail.com>
2019-08-23 03:18:27 +0900

    2798

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		int[] arr = new int[N];
+		int M = scan.nextInt();
+		int[] card = new int[N];
 		for(int i = 0; i < N; i++) {
-			arr[i] = scan.nextInt();
+			card[i] = scan.nextInt();
 		}
-		int max = -1000000;
-		int min = 1000000;
-		for(int i = 0; i < N; i++) {
-			if(max <= arr[i]) {
-				max = arr[i];
-			}
-			if(min >= arr[i]) {
-				min = arr[i];
+		scan.close();
+		Arrays.sort(card);
+		int result = 0;
+		for(int i = 0; i < N-2; i++) {
+			for(int j = i+1; j < N-1; j++) {
+				for(int k = j+1; k < N; k++) {
+					int sum = card[i] + card[j] + card[k];
+					if(sum <= M && result <= sum) {
+						result = sum;
+					}
+				}
 			}
 		}
-		System.out.print(min + " " + max);
+		System.out.print(result);
 	}
 }
