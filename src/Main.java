254a3128ec10f8896e2e222b455cfe83604d4806
ParkBeomMin <club20608@gmail.com>
2019-08-30 01:29:29 +0900

    7568

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,25 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
+		int arr[][] = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			arr[i][0] = scan.nextInt();
+			arr[i][1] = scan.nextInt();
+		}
 		scan.close();
-		int result = N;
-	for(int i = 0; i < N; i++) {
-		int sum = 0;
-		if(i / 100000 != 0) {
-			sum = i + (i / 100000) + ((i%100000)/10000) + ((i%10000)/1000) + ((i%1000)/100) + ((i%100)/10) + i%10;
-		}else if(i / 10000 != 0) {
-			sum = i + ((i%100000)/10000) + ((i%10000)/1000) + ((i%1000)/100) + ((i%100)/10) + i%10;
-		}else if(i / 1000 != 0) {
-			sum = i + ((i%10000)/1000) + ((i%1000)/100) + ((i%100)/10) + i%10;
-		}else if(i / 100 != 0) {
-			sum = i + ((i%1000)/100) + ((i%100)/10) + i%10;
-		}else if( i / 10 != 0) {
-			sum = i + ((i%100)/10) + i%10;
-		}else {
-			sum = i + i%10;
+		int result[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			int weight = arr[i][0];
+			int height = arr[i][1];
+			for (int j = 0; j < N; j++) {
+				if (weight < arr[j][0] && height < arr[j][1]) {
+					result[i] = result[i] + 1;
+				}
+			}
 		}
-		if(sum == N && result > i) {
-			result = i;
+		for (int i = 0; i < N; i++) {
+			System.out.print(result[i]+1 + " ");
 		}
-	}
-	if(result == N) {
-		result = 0;
-	}
-		System.out.print(result);
+
 	}
 }
