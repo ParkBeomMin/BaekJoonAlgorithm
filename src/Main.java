5f6caa91ce4e292c49d5b2e0e994244d0e6a244a
ParkBeomMin <club20608@gmail.com>
2019-09-06 09:49:56 +0900

    2750

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int first[] = {5000000, 3000000, 2000000, 500000, 300000, 100000};
-		int second[] = {5120000, 2560000, 1280000, 640000, 320000};
-		int T = scan.nextInt();
-		int input[][] = new int[T][2];
-		int result[] = new int[T];
-		for(int i = 0; i < T; i++) {
-			for(int j = 0; j < 2; j++) {
-				input[i][j] = scan.nextInt();
-			}
+	public static void main(String[] args) throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		scan.close();
-		for(int i = 0; i < T; i++) {
-			int a = input[i][0];
-			int b = input[i][1];
-			if(a <= 21 && a > 0) {
-				if( a == 1) {
-					result[i] += first[0];
-				}else if( a > 1 && a <= 3 ) {
-					result[i] += first[1];
-				}else if( a > 3 && a <= 6 ) {
-					result[i] += first[2];
-				}else if( a > 6 && a <= 10 ) {
-					result[i] += first[3];
-				}else if( a > 10 && a <= 15 ) {
-					result[i] += first[4];
-				}else if( a > 15 && a <= 21 ) {
-					result[i] += first[5];
-				}
-			}
-			if(b <= 31 && b > 0) {
-				if( b == 1) {
-					result[i] += second[0];
-				}else if( b > 1 && b <= 3 ) {
-					result[i] += second[1];
-				}else if( b > 3 && b <= 7 ) {
-					result[i] += second[2];
-				}else if( b > 7 && b <= 15 ) {
-					result[i] += second[3];
-				}else if( b > 15 && b <= 31 ) {
-					result[i] += second[4];
+		for(int i = 0; i < N-1; i++) {
+			for(int j = i+1; j < N; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
 				}
 			}
 		}
-		
-		for(int i = 0; i < T; i++) {
-			System.out.println(result[i]);
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 }
