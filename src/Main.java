dbe48f28cf51b5448e37663efa4e0841b78fc857
ParkBeomMin <club20608@gmail.com>
2019-09-25 20:38:42 +0900

    1260

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,67 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int first[] = {5000000, 3000000, 2000000, 500000, 300000, 100000};
-		int second[] = {5120000, 2560000, 1280000, 640000, 320000};
-		int T = scan.nextInt();
-		int input[][] = new int[T][2];
-		int result[] = new int[T];
-		for(int i = 0; i < T; i++) {
-			for(int j = 0; j < 2; j++) {
-				input[i][j] = scan.nextInt();
-			}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		InputStreamReader k = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(k);
+		String input = br.readLine();
+		String[] inputArr = input.split(" ");
+		int n = Integer.parseInt(inputArr[0]);
+		int m = Integer.parseInt(inputArr[1]);
+		int v = Integer.parseInt(inputArr[2]);
+
+		int[][] arr = new int[n][n];
+		int[] visit = new int[n];
+
+		for (int i = 0; i < m; i++) {
+			String input_ = br.readLine();
+			String[] inputArr_ = input_.split(" ");
+			int x = Integer.parseInt(inputArr_[0]) - 1;
+			int y = Integer.parseInt(inputArr_[1]) - 1;
+			arr[x][y] = 1;
+			arr[y][x] = 1;
 		}
-		scan.close();
-		for(int i = 0; i < T; i++) {
-			int a = input[i][0];
-			int b = input[i][1];
-			if(a <= 21 && a > 0) {
-				if( a == 1) {
-					result[i] += first[0];
-				}else if( a > 1 && a <= 3 ) {
-					result[i] += first[1];
-				}else if( a > 3 && a <= 6 ) {
-					result[i] += first[2];
-				}else if( a > 6 && a <= 10 ) {
-					result[i] += first[3];
-				}else if( a > 10 && a <= 15 ) {
-					result[i] += first[4];
-				}else if( a > 15 && a <= 21 ) {
-					result[i] += first[5];
-				}
-			}
-			if(b <= 31 && b > 0) {
-				if( b == 1) {
-					result[i] += second[0];
-				}else if( b > 1 && b <= 3 ) {
-					result[i] += second[1];
-				}else if( b > 3 && b <= 7 ) {
-					result[i] += second[2];
-				}else if( b > 7 && b <= 15 ) {
-					result[i] += second[3];
-				}else if( b > 15 && b <= 31 ) {
-					result[i] += second[4];
+
+		dfs(arr, visit, v);
+		System.out.println();
+		visit = new int[n];
+		bfs(arr, visit, v);
+
+	}
+
+	static void dfs(int[][] arr, int[] visit, int v) {
+		System.out.print(v + " ");
+		visit[v - 1] = 1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[v - 1][i] == 1) {
+				if (visit[i] != 1) {
+					dfs(arr, visit, i + 1);
 				}
 			}
 		}
-		
-		for(int i = 0; i < T; i++) {
-			System.out.println(result[i]);
+	}
+
+	static void bfs(int[][] arr, int[] visit, int v) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.offer(v - 1);
+		visit[v - 1] = 1;
+		System.out.print(v + " ");
+
+		int temp;
+		while (!q.isEmpty()) {
+			temp = q.poll();
+			for (int j = 0; j < visit.length; j++) {
+				if (arr[temp][j] == 1 && visit[j] == 0) {
+					q.offer(j);
+					visit[j] = 1;
+					System.out.print((j + 1) + " ");
+				}
+			}
 		}
+
 	}
 }
