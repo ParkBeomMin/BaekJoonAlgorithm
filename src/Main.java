24fbc64b88c38164a05c150fb04f14a184d1874c
ParkBeomMin <club20608@gmail.com>
2019-09-26 10:17:13 +0900

    1003

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,26 @@ public class Main {
 		InputStreamReader k = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(k);
 		int n = Integer.parseInt(br.readLine());
-		String nums = br.readLine();
-		int result = 0;
-		for(int i = 0; i < n; i++) {
-			result += Integer.parseInt(String.valueOf(nums.charAt(i)));
+		
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			int[] dp_zero = new int[num+1];
+			int[] dp_one = new int[num+1];
+			
+			for (int j = 0; j <= num; j++) {
+				if(j == 0) {
+					dp_zero[j] = 1;
+					dp_one[j] = 0;
+				}else if(j == 1) {
+					dp_zero[j] = 0;
+					dp_one[j] = 1;
+				}else {
+					dp_zero[j] = dp_zero[j-1] + dp_zero[j-2];
+					dp_one[j] = dp_one[j-1] + dp_one[j-2];	
+				}
+				
+			}
+			System.out.println(dp_zero[num] + " " + dp_one[num]);
 		}
-		System.out.print(result);
 	}
 }
