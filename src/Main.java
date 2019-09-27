eae3d607f24f558ebabafc4649a00a7c0bd621cc
ParkBeomMin <club20608@gmail.com>
2019-09-27 09:44:15 +0900

    1904

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
 		InputStreamReader k = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(k);
 		int n = Integer.parseInt(br.readLine());
-		int tmp = 666;
-		while(n > 1) {
-			tmp++;
-			if(String.valueOf(tmp).contains("666")) {
-				n--;	
-			}
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 2;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] %= 15746;
 		}
-		System.out.print(tmp);
+		System.out.print(dp[n-1]);
 
 	}
 }
