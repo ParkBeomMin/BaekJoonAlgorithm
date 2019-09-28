56ac3d992b8439e9ea132f0353e9f957efa91478
ParkBeomMin <club20608@gmail.com>
2019-09-28 18:12:29 +0900

    2156

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		
+
 		int n = Integer.parseInt(br.readLine());
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 1; i <= n; i++) {
-			queue.offer(i);
+		int[] wine = new int[10001];
+		long[] dp = new long[10001];
+		for (int i = 1; i <= n; i++) {
+			wine[i] = Integer.parseInt(br.readLine());
 		}
-		while(queue.size() != 1) {
-			queue.poll();
-			queue.offer(queue.poll());
+		dp[1] = wine[1];
+		dp[2] = wine[1] + wine[2];
+		for (int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
 		}
-		System.out.print(queue.poll());
-		
+		System.out.print(dp[n]);
+
 	}
 }
