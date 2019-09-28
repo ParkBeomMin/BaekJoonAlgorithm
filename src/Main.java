9939fb09335338dd0e55895aa428c7479c67b317
ParkBeomMin <club20608@gmail.com>
2019-09-28 16:55:12 +0900

    1541

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String[] input = br.readLine().split("-");
-		int sum = 0;
-		for(int i = 0; i < input.length; i++) {
-			String tmp = input[i];
-			if(i == 0) {
-				if(tmp.contains("+")) {
-					for(String num : tmp.split("\\+")) {
-						sum += Integer.parseInt(num);
-					}
-				}else {
-					sum += Integer.parseInt(tmp);
-				}
-			}else {
-				if(tmp.contains("+")) {
-					for(String num : tmp.split("\\+")) {
-						sum -= Integer.parseInt(num);
-					}
-				}else {
-					sum -= Integer.parseInt(tmp);
-				}
-			}
-		}
 		
+		int k = Integer.parseInt(br.readLine());
+		int sum = 0;
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < k; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0) {
+				stack.pop();
+			}else {
+				stack.push(num);
+			}
+			
+		}
+		while(!stack.isEmpty()) {
+			sum += stack.pop();
+		}
 		System.out.print(sum);
 	}
 }
