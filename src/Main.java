f5021f4bacd328348d0692e2d3ae19d67ab49d32
ParkBeomMin <club20608@gmail.com>
2019-09-28 12:06:53 +0900

    9461

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,18 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader k = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(k);
-		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[n + 1];
-		dp[0] = 1;
-		dp[1] = 2;
-		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];
-			dp[i] %= 15746;
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			int n = Integer.parseInt(br.readLine());
+			long[] dp = new long[101];
+			dp[0] = 1;
+			dp[1] = 1;
+			dp[2] = 1;
+			for (int j = 3; j < 101; j++) {
+				dp[j] = dp[j - 2] + dp[j - 3];
+			}
+			System.out.println(dp[n - 1]);
 		}
-		System.out.print(dp[n-1]);
 
 	}
 }
