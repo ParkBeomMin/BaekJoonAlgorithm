6302c04377480e352190e8cf19e0aa8d4876ca63
ParkBeomMin <club20608@gmail.com>
2019-09-28 17:17:06 +0900

    2164

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		
-		int k = Integer.parseInt(br.readLine());
-		int sum = 0;
-		Stack<Integer> stack = new Stack<>();
-		for(int i = 0; i < k; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if(num == 0) {
-				stack.pop();
-			}else {
-				stack.push(num);
-			}
-			
+		int n = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 1; i <= n; i++) {
+			queue.offer(i);
 		}
-		while(!stack.isEmpty()) {
-			sum += stack.pop();
+		while(queue.size() != 1) {
+			queue.poll();
+			queue.offer(queue.poll());
 		}
-		System.out.print(sum);
+		System.out.print(queue.poll());
+		
 	}
 }
