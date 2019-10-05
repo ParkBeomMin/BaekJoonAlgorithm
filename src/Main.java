a9f950552d7f9686d5228cc2ce55898ea25a40b1
ParkBeomMin <club20608@gmail.com>
2019-10-05 14:43:36 +0900

    2108

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,49 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[10001];
+		int[] arr = new int[8001];
+		int[] sorted = new int[n];
+		int sortedIndex = 0;
+		int maxCnt = 0;
+		int maxCntNum = -1;
+		boolean isSecond = false;
 		for (int i = 0; i < n; i++) {
 			int input = Integer.parseInt(br.readLine());
-			arr[input]++;
+			arr[input + 4000]++;
 		}
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		 
-		for (int i = 1; i < arr.length; i++) {
-			for(int j = 0; j < arr[i]; j++) {
-				bw.write(Integer.toString(i) + "\n");
+		for (int i = 0; i < arr.length; i++) {
+			if (maxCnt < arr[i]) {
+				maxCnt = arr[i];
+				maxCntNum = i;
+				isSecond = true;
+
+			} else if (maxCnt == arr[i]) {
+				if (isSecond) {
+					maxCntNum = i;
+					isSecond = false;
+				}
 			}
 		}
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i]; j++) {
+				sorted[sortedIndex] = i - 4000;
+				sortedIndex++;
+			}
+		}
+		double sum = 0;
+		for (int i = 0; i < sorted.length; i++) {
+			sum += sorted[i];
+		}
+		double average = sum / sorted.length;
+		bw.write(String.valueOf((int) Math.round(average)) + "\n");
+		bw.write(String.valueOf(sorted[sorted.length / 2]) + "\n");
+		bw.write(String.valueOf(maxCntNum - 4000) + "\n");
+		bw.write(String.valueOf(Math.abs(sorted[sorted.length - 1] - sorted[0])));
+
 		bw.close();
 		br.close();
 	}
