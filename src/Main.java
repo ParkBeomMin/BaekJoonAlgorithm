938fc26f1922334be913278a3720982c6e88eb47
ParkBeomMin <club20608@gmail.com>
2019-10-05 13:25:57 +0900

    10989

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,27 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
+		int[] arr = new int[10001];
 		for (int i = 0; i < n; i++) {
 			int input = Integer.parseInt(br.readLine());
-			arr[i] = input;
+			arr[input]++;
 		}
-
-		quickSort(arr, 0, arr.length - 1);
-		for (int i = 0; i < n; i++) {
-			System.out.println(arr[i]);
-		}
-	}
-
-	static void quickSort(int[] arr, int left, int right) {
-		int pivot = arr[(left + right) / 2];
-		int mLeft = left;
-		int mRight = right;
-		while (mLeft <= mRight) {
-			while (pivot > arr[mLeft]) {
-				mLeft++;
-			}
-			while (pivot < arr[mRight]) {
-				mRight--;
-			}
-			if (mLeft <= mRight) {
-				int tmp = arr[mLeft];
-				arr[mLeft] = arr[mRight];
-				arr[mRight] = tmp;
-				mLeft++;
-				mRight--;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		 
+		for (int i = 1; i < arr.length; i++) {
+			for(int j = 0; j < arr[i]; j++) {
+				bw.write(Integer.toString(i) + "\n");
 			}
 		}
-		if (left < mRight) {
-			quickSort(arr, left, mRight);
-		}
-		if (mLeft < right) {
-			quickSort(arr, mLeft, right);
-		}
+		bw.close();
+		br.close();
 	}
 }
