0a93cced8036945f1d797fb4f48aacf5ff73294f
ParkBeomMin <club20608@gmail.com>
2019-10-06 22:25:18 +0900

    10814

--- src/Main.java
+++ src/Main.java
@@ -12,41 +12,40 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		MyWord[] words = new MyWord[n];
+		User[] users = new User[n];
 		for (int i = 0; i < n; i++) {
-			String input = br.readLine();
-			words[i] = new MyWord(input);
+			String[] input = br.readLine().split(" ");
+			users[i] = new User(i, Integer.parseInt(input[0]), input[1]);
 		}
-		Arrays.sort(words);
-		for (int i = 0; i < words.length; i++) {
-			String tmp = words[0].word;
-			if(i == 0) {
-				bw.write(words[i].word + "\n");
-			}
-			if (i > 0){
-				tmp = words[i - 1].word;
-				if (!tmp.equals(words[i].word)) {
-					bw.write(words[i].word + "\n");
-				}
-			}
+		Arrays.sort(users);
+		for (int i = 0; i < users.length; i++) {
+			bw.write(users[i].age + " " + users[i].name + "\n");
 		}
 
 		bw.close();
 		br.close();
 	}
 
-	static class MyWord implements Comparable<MyWord> {
-		String word;
+	static class User implements Comparable<User> {
+		int num;
+		int age;
+		String name;
 
-		public MyWord(String word) {
-			this.word = word;
+		public User(int num, int age, String name) {
+			this.num = num;
+			this.age = age;
+			this.name = name;
 		}
 
 		@Override
-		public int compareTo(MyWord o) {
-			if (this.word.length() >= o.word.length()) {
-				if (this.word.length() == o.word.length()) {
-					return this.word.compareTo(o.word);
+		public int compareTo(User o) {
+			if (this.age >= o.age) {
+				if (this.age == o.age) {
+					if (this.num >= o.num) {
+						return 1;
+					} else {
+						return -1;
+					}
 				} else {
 					return 1;
 				}
