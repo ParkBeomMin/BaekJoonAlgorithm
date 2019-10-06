e8400479d55c2579b9b8fe91982cd228ea6333da
ParkBeomMin <club20608@gmail.com>
2019-10-06 21:11:44 +0900

    11650

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -11,47 +12,45 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[8001];
-		int[] sorted = new int[n];
-		int sortedIndex = 0;
-		int maxCnt = 0;
-		int maxCntNum = -1;
-		boolean isSecond = false;
-		for (int i = 0; i < n; i++) {
-			int input = Integer.parseInt(br.readLine());
-			arr[input + 4000]++;
+		Point[] points = new Point[n];
+		for(int i = 0; i < n; i++) {
+			String[] input = br.readLine().split(" ");
+			points[i] = new Point(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
 		}
-		for (int i = 0; i < arr.length; i++) {
-			if (maxCnt < arr[i]) {
-				maxCnt = arr[i];
-				maxCntNum = i;
-				isSecond = true;
-
-			} else if (maxCnt == arr[i]) {
-				if (isSecond) {
-					maxCntNum = i;
-					isSecond = false;
-				}
-			}
+		Arrays.sort(points);
+		for(int i = 0; i < n; i++) {
+			System.out.println(points[i].x + " " + points[i].y);
 		}
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i]; j++) {
-				sorted[sortedIndex] = i - 4000;
-				sortedIndex++;
-			}
-		}
-		double sum = 0;
-		for (int i = 0; i < sorted.length; i++) {
-			sum += sorted[i];
-		}
-		double average = sum / sorted.length;
-		bw.write(String.valueOf((int) Math.round(average)) + "\n");
-		bw.write(String.valueOf(sorted[sorted.length / 2]) + "\n");
-		bw.write(String.valueOf(maxCntNum - 4000) + "\n");
-		bw.write(String.valueOf(Math.abs(sorted[sorted.length - 1] - sorted[0])));
 
 		bw.close();
 		br.close();
+	}
+
+	static class Point implements Comparable<Point> {
+		int x;
+		int y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			if (this.x >= o.x) {
+				if (this.x == o.x) {
+					if (this.y >= o.y) {
+						return 1;
+					} else {
+						return -1;
+					}
+				} else {
+					return 1;
+				}
+
+			} else {
+				return -1;
+			}
+		}
 	}
 }
