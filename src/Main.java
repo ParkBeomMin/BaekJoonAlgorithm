fef4418b64dbce7fee1e37ddfd0f7b6e9feba838
ParkBeomMin <club20608@gmail.com>
2019-10-08 22:39:32 +0900

    17298

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		for(int i = 0; i < n; i++) {
-			String[] input = br.readLine().split(" ");
-			int repeatNum = Integer.parseInt(input[0]);
-			String normalString = input[1];
-			System.out.println(makeRepeatString(normalString, repeatNum));
+		Stack<Integer> lStack = new Stack<>();
+		Stack<Integer> rStack = new Stack<>();
+		int[] nge = new int[n];
+		String[] input = br.readLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			lStack.push(Integer.parseInt(input[i]));
 		}
-		br.close();
-	}
-	
-	static String makeRepeatString(String s, int n) {
-		String result = "";
-		for(int i = 0; i < s.length(); i++) {
-			for(int j = 0; j < n; j++) {
-				result += s.substring(i, i+1);
+		for (int i = 0; i < n; i++) {
+			int lTmp = lStack.pop();
+			if (rStack.isEmpty()) {
+				nge[n - i - 1] = -1;
+			} else {
+				while (!rStack.isEmpty()) {
+					int rTmp = rStack.pop();
+					if (lTmp < rTmp) {
+						nge[n - i - 1] = rTmp;
+						rStack.push(rTmp);
+						rStack.push(lTmp);
+						break;
+					} else {
+						if (rStack.isEmpty()) {
+							nge[n - i - 1] = -1;
+							break;
+						} else {
+							rTmp = rStack.pop();
+						}
+
+					}
+				}
 			}
+			rStack.push(lTmp);
 		}
-		return result;
+		for (int i = 0; i < n; i++) {
+			System.out.print(nge[i] + " ");
+		}
+
+		br.close();
 	}
 
 }
