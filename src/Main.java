e7b5f1c785d25f4b3f4d2d88c5fd2cc764c7a03a
ParkBeomMin <club20608@gmail.com>
2019-10-09 15:25:57 +0900

    11053

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		Stack<Integer> lStack = new Stack<>();
-		Stack<Integer> rStack = new Stack<>();
-		int[] nge = new int[n];
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		int result = 1;
 		String[] input = br.readLine().split(" ");
-		for (int i = 0; i < n; i++) {
-			lStack.push(Integer.parseInt(input[i]));
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(input[i]);
 		}
-		for (int i = 0; i < n; i++) {
-			int lTmp = lStack.pop();
-			if (rStack.isEmpty()) {
-				nge[n - i - 1] = -1;
-			} else {
-				while (!rStack.isEmpty()) {
-					int rTmp = rStack.pop();
-					if (lTmp < rTmp) {
-						nge[n - i - 1] = rTmp;
-						rStack.push(rTmp);
-						rStack.push(lTmp);
-						break;
-					} else {
-						if (rStack.isEmpty()) {
-							nge[n - i - 1] = -1;
-							break;
-						} else {
-							rTmp = rStack.pop();
-						}
-
-					}
+		dp[0] = 1;
+		for(int i = 1; i < n; i++) {
+			dp[i] = 1;
+			for(int j = 0; j < i; j++) {
+				if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
 				}
 			}
-			rStack.push(lTmp);
+			result = Math.max(result, dp[i]);
 		}
-		for (int i = 0; i < n; i++) {
-			System.out.print(nge[i] + " ");
-		}
+		System.out.print(result);
+		
 
 		br.close();
 	}
