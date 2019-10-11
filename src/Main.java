030cfe5c73f069264ca48959340952d24ae118ea
ParkBeomMin <club20608@gmail.com>
2019-10-11 21:54:10 +0900

    1966

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
+/**
+ * https://qlyh8.tistory.com/153 참고함.
+ * @author parkbeommin
+ *
+ */
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		int[] dp = new int[n];
-		int result = 1;
-		String[] input = br.readLine().split(" ");
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(input[i]);
-		}
-		dp[0] = 1;
-		for(int i = 1; i < n; i++) {
-			dp[i] = 1;
-			for(int j = 0; j < i; j++) {
-				if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-					dp[i] = dp[j] + 1;
+		int t = Integer.parseInt(br.readLine()); // test case
+		for (int i = 0; i < t; i++) {
+
+			int cnt = 0;
+			String[] input = br.readLine().split(" ");
+			int n = Integer.parseInt(input[0]);
+			int m = Integer.parseInt(input[1]);
+			String[] priority = br.readLine().split(" ");
+			LinkedList<Integer> queue = new LinkedList<>();
+			
+			for (int j = 0; j < n; j++) {
+				queue.add(Integer.parseInt(priority[j]));
+			}
+
+			while (!queue.isEmpty()) {
+				boolean isPriority = true;
+				for (int j = 1; j < queue.size(); j++) {
+					if (queue.peek() < queue.get(j)) {
+						isPriority = false;
+						break;
+					}
+				}
+				if (isPriority) {
+					cnt++;
+					queue.poll();
+					if (m != 0) {
+						m -= 1;
+					}else {
+						break;
+					}
+				} else {
+					queue.add(queue.poll());
+					if (m == 0) {
+						m = queue.size() - 1;
+					} else {
+						m -= 1;
+					}
 				}
 			}
-			result = Math.max(result, dp[i]);
+			System.out.println(cnt);
+			queue.clear();
 		}
-		System.out.print(result);
-		
 
 		br.close();
 	}
-
 }
