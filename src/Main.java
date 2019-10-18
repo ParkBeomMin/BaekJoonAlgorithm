5b8f16367f51caca35885dcb1ae27a20c94e7991
ParkBeomMin <club20608@gmail.com>
2019-10-18 12:00:50 +0900

    5430

--- src/Main.java
+++ src/Main.java
@@ -8,116 +8,61 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int m = Integer.parseInt(input[1]);
-		MyDeque deque = new MyDeque();
-		int result = 0;
-		int[] finds = new int[m];
-		String[] findsInput = br.readLine().split(" ");
-		for (int i = 0; i < m; i++) {
-			finds[i] = Integer.parseInt(findsInput[i]);
-		}
-		for (int i = 1; i <= n; i++) {
-			deque.offerLast(i);
-		}
-		for (int i = 0; i < m; i++) {
-			result += deque.minMove(finds[i]);
-			deque.initMoveCnt();
-		}
-		System.out.print(result);
-	}
-
-	static class MyDeque {
-		Deque<Integer> deque;
-		int leftMove;
-		int rightMove;
-
-		public MyDeque() {
-			this.deque = new LinkedList<>();
-			this.leftMove = 0;
-			this.rightMove = 0;
-		}
-
-		void initMoveCnt() {
-			this.leftMove = 0;
-			this.rightMove = 0;
-		}
-
-		void offerLast(int value) {
-			this.deque.offerLast(value);
-		}
-
-		int minMove(int key) {
-			if (this.findLeftMove(key) <= this.findRightMove(key)) {
-				this.leftMove(this.leftMove);
-				this.pollFirst();
-				return this.leftMove;
+		int testCase = Integer.parseInt(br.readLine());
+		for (int i = 0; i < testCase; i++) {
+			String command = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			Deque<Integer> deque = new LinkedList<>();
+			String input = br.readLine();
+			input = input.substring(1, input.length() - 1);
+			String[] inputs = input.split(",");
+			boolean isError = false;
+			boolean isReverse = false;
+			for (int j = 0; j < inputs.length; j++) {
+				if (inputs[j].length() != 0) {
+					deque.offerLast(Integer.parseInt(inputs[j]));
+				}
+			}
+			for (int j = 0; j < command.length(); j++) {
+				if (command.substring(j, j + 1).equals("R")) {
+					isReverse = !isReverse;
+				} else {
+					if (deque.isEmpty()) {
+						isError = true;
+					} else {
+						if (isReverse) {
+							deque.pollLast();
+						} else {
+							deque.pollFirst();
+						}
+					}
+				}
+			}
+			if (isError) {
+				System.out.print("error");
 			} else {
-				this.rightMove(this.rightMove);
-				this.pollFirst();
-				return this.rightMove;
-			}
-		}
-
-		int findLeftMove(int key) {
-			while (true) {
-				if (this.findValue(key)) {
-					break;
+				System.out.print("[");
+				while (!deque.isEmpty()) {
+					if (isReverse) {
+						if (deque.size() == 1) {
+							System.out.print(deque.pollLast());
+						} else {
+							System.out.print(deque.pollLast() + ",");
+						}
+					} else {
+						if (deque.size() == 1) {
+							System.out.print(deque.pollFirst());
+						} else {
+							System.out.print(deque.pollFirst() + ",");
+						}
+					}
 				}
-				leftMove();
+				System.out.print("]");
 			}
-			rightMove(this.leftMove);
-			return this.leftMove;
-		}
+			System.out.println();
+			deque.clear();
 
-		int findRightMove(int key) {
-			while (true) {
-				if (this.findValue(key)) {
-					break;
-				}
-				rightMove();
-			}
-			leftMove(this.rightMove);
-			return this.rightMove;
-		}
-
-		void leftMove() {
-			this.deque.offerLast(this.deque.pollFirst());
-			this.leftMove++;
-		}
-
-		void leftMove(int cnt) {
-			while (cnt > 0) {
-				this.leftMove();
-				this.leftMove--;
-				cnt--;
-			}
-		}
-
-		void rightMove() {
-			this.deque.offerFirst(this.deque.pollLast());
-			this.rightMove++;
-		}
-
-		void rightMove(int cnt) {
-			while (cnt > 0) {
-				this.rightMove();
-				this.rightMove--;
-				cnt--;
-			}
-		}
-
-		int pollFirst() {
-			return this.deque.pollFirst();
-		}
-
-		boolean findValue(int key) {
-			boolean isFind = false;
-			if (this.deque.peekFirst() == key) {
-				isFind = true;
-			}
-			return isFind;
 		}
 	}
+
 }
