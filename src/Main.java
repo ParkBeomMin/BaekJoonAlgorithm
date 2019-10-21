1bdf64e1df9d42fd1b5f8b8cf9d4a31556e128c4
ParkBeomMin <club20608@gmail.com>
2019-10-21 13:14:36 +0900

    15651

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		int n = Integer.parseInt(br.readLine());
-		int[][] paper = new int[n][n];
-		int[] cnt = new int[3];
-		for (int i = 0; i < n; i++) {
-			String[] input = br.readLine().split(" ");
-			for (int j = 0; j < n; j++) {
-				paper[i][j] = Integer.parseInt(input[j]);
-			}
-		}
-		cut(paper, cnt);
-		System.out.println(cnt[0]);
-		System.out.println(cnt[1]);
-		System.out.println(cnt[2]);
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int m = Integer.parseInt(input[1]);
+		int[] arr = new int[m];
+		dfs(arr, 0, n);
+		bw.close();
 	}
 
-	static void cut(int[][] paper, int[] cnt) {
-		if (!isCut(paper, -1)) {
-			cnt[0]++;
-		} else if (!isCut(paper, 0)) {
-			cnt[1]++;
-		} else if (!isCut(paper, 1)) {
-			cnt[2]++;
+	static void dfs(int[] arr, int p, int n) throws IOException {
+		if (p == arr.length) {
+			for (int i = 0; i < arr.length; i++) {
+				bw.write(arr[i] + " ");
+			}
+			bw.write("\n");
 		} else {
-			cut(cut(paper, 0, paper.length / 3, 0, paper.length / 3), cnt); // 좌상단
-			cut(cut(paper, 0, paper.length / 3, paper.length / 3, 2 * paper.length / 3), cnt); // 중상단
-			cut(cut(paper, 0, paper.length / 3, 2 * paper.length / 3, paper.length), cnt); // 우상단
-			cut(cut(paper, paper.length / 3, 2 * paper.length / 3, 0, paper.length / 3), cnt); // 좌중단
-			cut(cut(paper, paper.length / 3, 2 * paper.length / 3, paper.length / 3, 2 * paper.length / 3), cnt); // 중중단
-			cut(cut(paper, paper.length / 3, 2 * paper.length / 3, 2 * paper.length / 3, paper.length), cnt); // 우중단
-			cut(cut(paper, 2 * paper.length / 3, paper.length, 0, paper.length / 3), cnt); // 좌하단
-			cut(cut(paper, 2 * paper.length / 3, paper.length, paper.length / 3, 2 * paper.length / 3), cnt); // 중하단
-			cut(cut(paper, 2 * paper.length / 3, paper.length, 2 * paper.length / 3, paper.length), cnt); // 우하단
-
-		}
-	}
-
-	static int[][] cut(int[][] paper, int rStart, int rEnd, int cStart, int cEnd) {
-		int[][] returnArr = new int[paper.length / 3][paper.length / 3];
-		for (int i = rStart; i < rEnd; i++) {
-			for (int j = cStart; j < cEnd; j++) {
-				returnArr[i - rStart][j - cStart] = paper[i][j];
+			for (int i = 1; i <= n; i++) {
+				arr[p] = i;
+				dfs(arr, p + 1, n);
 			}
 		}
-		return returnArr;
 	}
-
-	static boolean isCut(int[][] paper, int option) {// false면 안자르고, true면 자르거
-		boolean returnBool = true;
-		for (int i = 0; i < paper.length; i++) {
-			for (int j = 0; j < paper.length; j++) {
-				if (paper[i][j] != option) {
-					returnBool = false;
-					break;
-				}
-			}
-			if (!returnBool) {
-				break;
-			}
-		}
-		return !returnBool;
-	}
-
 }
