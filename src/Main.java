bd1ba43007638f11b52332fb7175f63417cc5ca8
ParkBeomMin <club20608@gmail.com>
2019-10-21 16:23:29 +0900

    10816

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,59 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int m = Integer.parseInt(input[1]);
-		int[] arr = new int[m];
-		dfs(arr, 0, n);
-		bw.close();
+		int n = Integer.parseInt(br.readLine());
+		String[] inputN = br.readLine().split(" ");
+		int m = Integer.parseInt(br.readLine());
+		String[] inputM = br.readLine().split(" ");
+		int[] cards = new int[n];
+		int[] nums = new int[m];
+		int index = -1;
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			cards[i] = Integer.parseInt(inputN[i]);
+		}
+		for (int i = 0; i < m; i++) {
+			nums[i] = Integer.parseInt(inputM[i]);
+		}
+		Arrays.sort(cards);
+		for (int i = 0; i < nums.length; i++) {
+			int lower = lowerSearch(cards, nums[i]);
+			int upper = upperSearch(cards, nums[i]);
+			System.out.print(upper - lower + " ");
+		}
 	}
 
-	static void dfs(int[] arr, int p, int n) throws IOException {
-		if (p == arr.length) {
-			for (int i = 0; i < arr.length; i++) {
-				bw.write(arr[i] + " ");
-			}
-			bw.write("\n");
-		} else {
-			for (int i = 1; i <= n; i++) {
-				arr[p] = i;
-				dfs(arr, p + 1, n);
+	static int lowerSearch(int[] cards, int target) {
+		int start = 0;
+		int end = cards.length;
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (cards[mid] >= target) {
+				end = mid;
+			} else {
+				start = mid + 1;
 			}
 		}
+		return start;
+	}
+
+	static int upperSearch(int[] cards, int target) {
+		int start = 0;
+		int end = cards.length;
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (cards[mid] <= target) {
+				start = mid + 1;
+			} else {
+				end = mid;
+			}
+		}
+		return start;
 	}
 }
