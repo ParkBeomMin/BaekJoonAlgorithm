a44ac93b8474e4b0c13c41ba9b0dec2a893fe4fa
ParkBeomMin <club20608@gmail.com>
2019-10-23 11:18:40 +0900

    2667

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int m = Integer.parseInt(input[1]);
-		int[] arr = new int[m];
-		dfs(arr, n, 0);
+		int n = Integer.parseInt(br.readLine());
+		int[][] area = new int[n][n];
+		int[][] visited = new int[n][n];
+		List<Integer> group = new ArrayList<>();
+		int groupId = 0;
+		for (int i = 0; i < n; i++) {
+			String input = br.readLine();
+			for (int j = 0; j < n; j++) {
+				area[i][j] = Integer.parseInt(input.substring(j, j + 1));
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (area[i][j] == 1 && visited[i][j] == 0) {
+					groupId++;
+					dfs(area, visited, group, groupId, i, j);
+				}
+			}
+		}
+		int[] result = new int[groupId];
+		Collections.sort(group);
+		for (int i = 1; i <= groupId; i++) {
+			for (int j = 0; j < group.size(); j++) {
+				if (group.get(j) == i) {
+					result[i-1]++;
+				}
+			}
+		}
+		Arrays.sort(result);
+		System.out.println(result.length);
+		for (int groupNum : result) {
+			System.out.println(groupNum);
+		}
+
 	}
 
-	static void dfs(int[] arr, int n, int p) {
-		if (p == arr.length) {
-			for (int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
-		} else {
-			for (int i = 1; i <= n; i++) {
-				if (p != 0) {
-					if (arr[p - 1] <= i) {
-						arr[p] = i;
-						dfs(arr, n, p + 1);
-					}
-				} else {
-					arr[p] = i;
-					dfs(arr, n, p + 1);
+	static void dfs(int[][] area, int[][] visited, List<Integer> group, int groupId, int x, int y) {
+		int[] dx = { -1, 0, 1, 0 }; // 좌상우하
+		int[] dy = { 0, -1, 0, 1 };
+		visited[x][y] = 1;
+		group.add(groupId);
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && ny >= 0 && nx < area.length && ny < area.length) {
+				if (area[nx][ny] == 1 && visited[nx][ny] == 0) {
+					dfs(area, visited, group, groupId, nx, ny);
 				}
 
 			}
 		}
+
 	}
 
 }
