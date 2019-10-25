fa7a8e652811244019b744e95c4a9d95a38a4764
ParkBeomMin <club20608@gmail.com>
2019-10-25 15:38:54 +0900

    14620

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
+	static int min = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int m = Integer.parseInt(input[1]);
-		int[][] tomato = new int[m][n];
-		for (int i = 0; i < m; i++) {
-			String[] status = br.readLine().split(" ");
+		int n = Integer.parseInt(br.readLine());
+		int[][] cost = new int[n][n];
+		boolean[][] visited = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			String[] input = br.readLine().split(" ");
 			for (int j = 0; j < n; j++) {
-				tomato[i][j] = Integer.parseInt(status[j]);
+				cost[i][j] = Integer.parseInt(input[j]);
 			}
 		}
-		bfs(tomato);
+
+		dfs(cost, visited, 0, 0);
+		System.out.println(min);
+
 	}
 
-	static void bfs(int[][] tomato) {
-		int[] dx = { -1, 0, 1, 0 };
-		int[] dy = { 0, -1, 0, 1 };
-		Queue<Dot> queue = new LinkedList<>();
-		for (int i = 0; i < tomato.length; i++) {
-			for (int j = 0; j < tomato[i].length; j++) {
-				if (tomato[i][j] == 1) {
-					queue.add(new Dot(i, j));
+	static void dfs(int[][] cost, boolean[][] visited, int depth, int sum) {
+		int[] dx = { 0, -1, 0, 1, 0 };
+		int[] dy = { 0, 0, -1, 0, 1 };
+		if (depth == 3) {
+			min = Math.min(min, sum);
+		} else {
+			for (int i = 1; i < cost.length - 1; i++) {
+				for (int j = 1; j < cost.length - 1; j++) {
+					if (check(visited, i, j)) {
+						for (int k = 0; k < 5; k++) {
+							visited[i + dx[k]][j + dy[k]] = true;
+						}
+						for (int k = 0; k < 5; k++) {
+							sum += cost[i + dx[k]][j + dy[k]];
+						}
+						dfs(cost, visited, depth + 1, sum);
+						for (int k = 0; k < 5; k++) {
+							visited[i + dx[k]][j + dy[k]] = false;
+							sum -= cost[i + dx[k]][j + dy[k]];
+						}
+					}
 				}
 			}
 		}
-		while (!queue.isEmpty()) {
-			Dot dot = queue.poll();
-			for (int i = 0; i < 4; i++) {
-				int x = dot.x + dx[i];
-				int y = dot.y + dy[i];
-				if (x < 0 || x >= tomato.length || y < 0 || y >= tomato[0].length) {
-					continue;
-				}
-				if (tomato[x][y] != 0) {
-					continue;
-				} else {
-					queue.add(new Dot(x, y));
-					tomato[x][y] = tomato[dot.x][dot.y] + 1;
-				}
-			}
-		}
-		System.out.print(statusCheck(tomato));
 	}
 
-	static int statusCheck(int[][] tomato) {
-		int tmp = 0;
-		for (int i = 0; i < tomato.length; i++) {
-			for (int j = 0; j < tomato[i].length; j++) {
-				if (tomato[i][j] == 0) {
-					return -1;
-				}
-				if (tmp < tomato[i][j]) {
-					tmp = tomato[i][j];
-				}
+	static boolean check(boolean[][] visited, int x, int y) {
+		boolean tmp = true;
+		int[] dx = { 0, -1, 0, 1, 0 };
+		int[] dy = { 0, 0, -1, 0, 1 };
+		for (int i = 0; i < 5; i++) {
+			if (visited[x + dx[i]][y + dy[i]]) {
+				tmp = false;
 			}
 		}
-		return tmp - 1;
-	}
-
-	static class Dot {
-		int x;
-		int y;
-
-		public Dot(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
+		return tmp;
 	}
 }
