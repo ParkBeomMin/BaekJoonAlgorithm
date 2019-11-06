991cd9af14a00c3da47ac02e644bb04c941f4e21
ParkBeomMin <club20608@gmail.com>
2019-11-06 16:08:59 +0900

    7569

--- src/Main.java
+++ src/Main.java
@@ -9,60 +9,93 @@ public class Main {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int m = Integer.parseInt(input[1]);
-		int[][] maze = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			String rows = br.readLine();
-			for (int j = 0; j < m; j++) {
-				maze[i][j] = Integer.parseInt(rows.substring(j, j + 1));
+		int col = Integer.parseInt(input[0]);
+		int row = Integer.parseInt(input[1]);
+		int h = Integer.parseInt(input[2]);
+		int[][][] farm = new int[row][col][h];
+		for (int k = 0; k < h; k++) {
+			for (int i = 0; i < row; i++) {
+				String[] tomatos = br.readLine().split(" ");
+				for (int j = 0; j < col; j++) {
+					farm[i][j][k] = Integer.parseInt(tomatos[j]);
+				}
 			}
 		}
-		bfs(maze, n, m);
+
+		bfs(farm, row, col, h);
 
 	}
 
-	static void bfs(int[][] maze, int n, int m) {
-		int[] dx = { -1, 0, 1, 0 };
-		int[] dy = { 0, -1, 0, 1 };
-		boolean[][] visited = new boolean[n][m];
+	static void bfs(int[][][] farm, int row, int col, int h) {
+		int[] dx = { -1, 1, 0, 0, 0, 0 };
+		int[] dy = { 0, 0, -1, 1, 0, 0 };
+		int[] dz = { 0, 0, 0, 0, -1, 1 };
+
+		boolean[][][] visited = new boolean[row][col][h];
 		Queue<Point> queue = new LinkedList<>();
-		int cnt = 1;
-		int result = 0;
-		queue.offer(new Point(0, 0, cnt));
-		visited[0][0] = true;
+		for (int k = 0; k < h; k++) {
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					if (farm[i][j][k] == 1) {
+						queue.offer(new Point(i, j, k));
+					}
+				}
+			}
+		}
+
 		while (!queue.isEmpty()) {
 			Point tmp = queue.poll();
 			int x = tmp.x;
 			int y = tmp.y;
-			result = tmp.distance;
-			if (x == n - 1 && y == m - 1) {
-				break;
-			}
-			for (int i = 0; i < 4; i++) {
+			int z = tmp.z;
+			visited[x][y][z] = true;
+			for (int i = 0; i < 6; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-				if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				int nz = z + dz[i];
+				if (nx < 0 || nx >= row || ny < 0 || ny >= col || nz < 0 || nz >= h) {
 					continue;
 				}
-				if (maze[nx][ny] == 1 && visited[nx][ny] == false) {
-					visited[nx][ny] = true;
-					queue.offer(new Point(nx, ny, result + 1));
+				if (farm[nx][ny][nz] != 0) {
+					continue;
+				} else {
+					farm[nx][ny][nz] = farm[x][y][z] + 1;
+					queue.offer(new Point(nx, ny, nz));
 				}
 			}
 		}
-		System.out.println(result);
+
+		System.out.println(check(farm, row, col, h));
+	}
+
+	static int check(int[][][] farm, int n, int m, int h) {
+		int tmp = 0;
+		for (int k = 0; k < h; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					if (farm[i][j][k] == 0) {
+						return -1;
+					} else {
+						if (tmp < farm[i][j][k]) {
+							tmp = farm[i][j][k];
+						}
+					}
+				}
+			}
+		}
+
+		return tmp - 1;
 	}
 
 	static class Point {
 		int x;
 		int y;
-		int distance;
+		int z;
 
-		public Point(int x, int y, int distance) {
+		public Point(int x, int y, int z) {
 			this.x = x;
 			this.y = y;
-			this.distance = distance;
+			this.z = z;
 		}
 	}
 }
