b81295f01f0986d5c8b0dd775c547bcc488914f9
ParkBeomMin <club20608@gmail.com>
2019-11-06 16:42:29 +0900

    1697

--- src/Main.java
+++ src/Main.java
@@ -8,94 +8,50 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
+		int max = 100000;
 		String[] input = br.readLine().split(" ");
-		int col = Integer.parseInt(input[0]);
-		int row = Integer.parseInt(input[1]);
-		int h = Integer.parseInt(input[2]);
-		int[][][] farm = new int[row][col][h];
-		for (int k = 0; k < h; k++) {
-			for (int i = 0; i < row; i++) {
-				String[] tomatos = br.readLine().split(" ");
-				for (int j = 0; j < col; j++) {
-					farm[i][j][k] = Integer.parseInt(tomatos[j]);
-				}
-			}
-		}
-
-		bfs(farm, row, col, h);
-
+		int n = Integer.parseInt(input[0]);
+		int k = Integer.parseInt(input[1]);
+		bfs(n, k, max);
 	}
 
-	static void bfs(int[][][] farm, int row, int col, int h) {
-		int[] dx = { -1, 1, 0, 0, 0, 0 };
-		int[] dy = { 0, 0, -1, 1, 0, 0 };
-		int[] dz = { 0, 0, 0, 0, -1, 1 };
-
-		boolean[][][] visited = new boolean[row][col][h];
+	static void bfs(int n, int k, int max) {
 		Queue<Point> queue = new LinkedList<>();
-		for (int k = 0; k < h; k++) {
-			for (int i = 0; i < row; i++) {
-				for (int j = 0; j < col; j++) {
-					if (farm[i][j][k] == 1) {
-						queue.offer(new Point(i, j, k));
-					}
-				}
-			}
-		}
-
+		int sec = 0;
+		boolean[] visited = new boolean[max + 1];
+		queue.offer(new Point(n, sec));
+		visited[n] = true;
 		while (!queue.isEmpty()) {
 			Point tmp = queue.poll();
 			int x = tmp.x;
-			int y = tmp.y;
-			int z = tmp.z;
-			visited[x][y][z] = true;
-			for (int i = 0; i < 6; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				int nz = z + dz[i];
-				if (nx < 0 || nx >= row || ny < 0 || ny >= col || nz < 0 || nz >= h) {
-					continue;
+			sec = tmp.second;
+			if (x == k) {
+				break;
+			} else {
+				if (x - 1 >= 0 && !visited[x - 1]) {
+					queue.offer(new Point(x - 1, sec + 1));
+					visited[x - 1] = true;
 				}
-				if (farm[nx][ny][nz] != 0) {
-					continue;
-				} else {
-					farm[nx][ny][nz] = farm[x][y][z] + 1;
-					queue.offer(new Point(nx, ny, nz));
+				if (x + 1 <= max && !visited[x + 1]) {
+					queue.offer(new Point(x + 1, sec + 1));
+					visited[x + 1] = true;
 				}
-			}
-		}
-
-		System.out.println(check(farm, row, col, h));
-	}
-
-	static int check(int[][][] farm, int n, int m, int h) {
-		int tmp = 0;
-		for (int k = 0; k < h; k++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (farm[i][j][k] == 0) {
-						return -1;
-					} else {
-						if (tmp < farm[i][j][k]) {
-							tmp = farm[i][j][k];
-						}
-					}
+				if (2 * x <= max && !visited[2 * x]) {
+					queue.offer(new Point(2 * x, sec + 1));
+					visited[2 * x] = true;
 				}
 			}
 		}
-
-		return tmp - 1;
+		System.out.println(sec);
 	}
 
 	static class Point {
 		int x;
-		int y;
-		int z;
+		int second;
 
-		public Point(int x, int y, int z) {
+		public Point(int x, int second) {
 			this.x = x;
-			this.y = y;
-			this.z = z;
+			this.second = second;
 		}
 	}
 }
