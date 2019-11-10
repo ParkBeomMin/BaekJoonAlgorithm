65363b083c0ab3d54968f8de9cc0382d593fbc73
ParkBeomMin <club20608@gmail.com>
2019-11-10 17:53:22 +0900

    11286

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -9,10 +10,29 @@ public class Main {
 		BufferedReader br = new BufferedReader(isr);
 		int n = Integer.parseInt(br.readLine());
 		int answer = 0;
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				if (Math.abs(o1) >= Math.abs(o2)) { // 너(o2)가 나(o1)보다 작냐? 맞으면 앞으로(오름차순)
+					if (Math.abs(o1) == Math.abs(o2)) {
+						if (o1 >= o2) {
+							return 1;
+						} else {
+							return -1;
+						}
+					} else {
+						return 1;
+					}
+				} else {
+					return -1;
+				}
+			}
+
+		});
 		for (int i = 0; i < n; i++) {
 			int num = Integer.parseInt(br.readLine());
-			if (num > 0) {
+			if (num != 0) {
 				pq.offer(num);
 			} else {
 				if (!pq.isEmpty()) {
