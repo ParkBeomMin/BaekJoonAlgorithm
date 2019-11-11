9e7527efd2a348adcf6b310422845b74dcc1b505
ParkBeomMin <club20608@gmail.com>
2019-11-11 13:56:50 +0900

    17211

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,33 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		System.out.print(comb(M - 1, M - N));
-	}
-
-	static int comb(int n, int m) {
-		if (n == m || m == 0) {
-			return 1;
-		} else {
-			return comb(n - 1, m - 1) + comb(n - 1, m);
+		String[] input = br.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int feel = Integer.parseInt(input[1]);
+		double good = 0;
+		double bad = 0;
+		double[] probs = new double[4];
+		String[] probInput = br.readLine().split(" ");
+		for (int i = 0; i < probs.length; i++) {
+			probs[i] = Double.parseDouble(probInput[i]);
 		}
+		if (feel == 0) {// 좋은날
+			good = probs[0];
+			bad = probs[1];
+		} else {
+			good = probs[2];
+			bad = probs[3];
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			double goodTmp = good;
+			double badTmp = bad;
+			good = goodTmp * probs[0] + badTmp * probs[2];
+			bad = goodTmp * probs[1] + badTmp * probs[3];
+		}
+
+		System.out.println(Math.round(good * 1000));
+		System.out.println(Math.round(bad * 1000));
 	}
 
 }
